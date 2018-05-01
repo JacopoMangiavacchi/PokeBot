560f09d8d93e7d650f9be5c8789843a558416563
Jacopo <jmangia@me.com>
2018-05-01 16:35:23 -0700

    no message

--- server/pokeapi/index.js
+++ server/pokeapi/index.js
@@ -17,50 +17,17 @@ const typeCache = new NodeCache({ stdTTL: 0, checkperiod: 0 });
 
 const port = 4000
 
-app.listen(port, function(){
+app.listen(port, function() {
     console.log(`Server listening on port ${port}`);
 });
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get('/pokemon/:key', function(req , res){
+app.get('/pokemon/:key', function(req , res) {
     var key = req.params.key;
-    var pokemon = {}
-
-    callPokeAPI(ApiEnum.POKEMON, key).then(resp => {
-        var types = [];
-        resp.types.forEach(element => {
-            types.push(element.type.name);
-        });
-
-        var species = resp.species.name;
-
-        pokemon = {
-                    "id": resp.id,
-                    "name": resp.name,
-                    "height": resp.height,
-                    "weight": resp.weight,
-                    "types": types,
-                    "thumbnail": `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${resp.id}.png`,
-                    "image": `https://img.pokemondb.net/artwork/${resp.name}.jpg`
-                };
-
-        return callPokeAPI(ApiEnum.HABITAT, species);
-    }).then(resp => {
-        pokemon.habitats = resp.habitat.name;
-
-        var flavors = resp.flavor_text_entries;
-        var flavorText = "";
-
-        flavors.forEach(element => {
-            if(element.language.name == "en") {
-                flavorText = element.flavor_text.replace(/(?:\r\n|\r|\n|\f)/g, ' ');
-            }
-        });
-
-        pokemon.flavorText = flavorText;
-
+    
+    getPokemon(key).then(pokemon => {
         res.json(pokemon);    
         res.end();   
     }).catch(err => {
@@ -72,15 +39,10 @@ app.get('/pokemon/:key', function(req , res){
     });
 });
 
-app.get('/type/:key', function(req , res){
+app.get('/type/:key', function(req , res) {
     var key = req.params.key;
 
-    callPokeAPI(ApiEnum.TYPE, key).then(resp => {
-        var pokemons = [];
-        resp.pokemon.forEach(element => {
-            pokemons.push(element.pokemon.name);
-        });
-
+    getAllOfType(key).then(pokemons => {
         res.json(pokemons);    
         res.end();   
     }).catch(err => {
@@ -91,6 +53,69 @@ app.get('/type/:key', function(req , res){
         res.end();
     });
 });
+
+
+function getPokemon(key) {
+    return new Promise((resolve, reject) => {
+        var pokemon = {}
+
+        callPokeAPI(ApiEnum.POKEMON, key).then(resp => {
+            var types = [];
+            resp.types.forEach(element => {
+                types.push(element.type.name);
+            });
+    
+            var species = resp.species.name;
+    
+            pokemon = {
+                        "id": resp.id,
+                        "name": resp.name,
+                        "height": resp.height,
+                        "weight": resp.weight,
+                        "types": types,
+                        "thumbnail": `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${resp.id}.png`,
+                        "image": `https://img.pokemondb.net/artwork/${resp.name}.jpg`
+                    };
+    
+            return callPokeAPI(ApiEnum.HABITAT, species);
+        }).then(resp => {
+            pokemon.habitats = resp.habitat.name;
+    
+            var flavors = resp.flavor_text_entries;
+            var flavorText = "";
+    
+            flavors.forEach(element => {
+                if(element.language.name == "en") {
+                    flavorText = element.flavor_text.replace(/(?:\r\n|\r|\n|\f)/g, ' ');
+                }
+            });
+    
+            pokemon.flavorText = flavorText;
+    
+            resolve(pokemon);
+        }).catch(err => {
+            reject(err);
+        });
+    });
+}
+
+
+function getAllOfType(key) {
+    return new Promise((resolve, reject) => {
+        var pokemon = {}
+
+        callPokeAPI(ApiEnum.TYPE, key).then(resp => {
+            var pokemons = [];
+            resp.pokemon.forEach(element => {
+                pokemons.push(element.pokemon.name);
+            });
+    
+            resolve(pokemons)
+        }).catch(err => {
+            reject(err);
+        });
+    });
+}
 
 
 function callPokeAPI(apiEnum, key) {
@@ -121,8 +146,6 @@ function callPokeAPI(apiEnum, key) {
     value = cache.get(key);
     if ( value == undefined ){
         return new Promise((resolve, reject) => {
-            console.log(`searching ${apiCommand} ${key}`)
-
             request({
                         method: 'GET',
                         uri: `https://pokeapi.co/api/v2/${apiCommand}/${key}/`,
@@ -144,7 +167,6 @@ function callPokeAPI(apiEnum, key) {
     }
     else {
         return new Promise((resolve, reject) => {
-            console.log(`found ${apiCommand} ${key}`)
             resolve(value);
         });
     }
