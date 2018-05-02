68f7ed93ae745c9c0b5ace70548c17bbbd68abe3
Jacopo <jmangia@me.com>
2018-05-01 22:20:48 -0700

    no message

---
+++ client/PokeBot/PokeBot/ViewController.swift
@@ -0,0 +1,56 @@
+//
+//  ViewController.swift
+//  PokeBot
+//
+//  Created by Jacopo Mangiavacchi on 5/1/18.
+//  Copyright © 2018 Jacopo Mangiavacchi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        let layout = UICollectionViewFlowLayout()
+        layout.sectionInset = UIEdgeInsets(top: 0, left: 10, bottom: 0, right: 10)
+        layout.minimumInteritemSpacing = 0.0
+        layout.minimumLineSpacing = 10.0
+        layout.itemSize = CGSize(width: collectionView.frame.width - 20, height: collectionView.frame.height)
+        layout.scrollDirection = .horizontal
+        
+        collectionView.collectionViewLayout = layout
+        collectionView.delegate = self
+        collectionView.dataSource = self
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: UICollectionViewDataSource
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of items
+        return 5
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath)
+        
+        // Configure the cell
+        
+        return cell
+    }
+
+}
+
